0c50f68ef70369b1b32730ba4d294120611f9650
haydenmcp <hayden.mcparlane@gmail.com>
2018-10-26 02:31:05 -0500

    removed unused import

--- packages/deep-microservice-root/index.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line
-const fetch = require('node-fetch');
-
-// eslint-disable-next-line
-var whatever = 'thing';
--- packages/deep-microservice-root/test/index.unit.js
+++ packages/deep-microservice-root/test/index.unit.js
@@ -1,7 +1,8 @@
 const expect = require('chai').expect;
 
+// TODO: Enable testing
 describe('root microservice', () => {
-    it('sample test', () => {
-        expect(2).to.equal(1);
-    });
+  it('sample test', () => {
+    expect(2).to.equal(1);
+  });
 });
