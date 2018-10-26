fb2e072c8a71bc11e8f0f2b0634cc36b4ade1698
haydenmcp <hayden.mcparlane@gmail.com>
2018-10-26 07:01:21 -0500

    fixed dummy test error

--- packages/deep-microservice-root/test/index.unit.js
+++ packages/deep-microservice-root/test/index.unit.js
@@ -3,6 +3,6 @@ const expect = require('chai').expect;
 // TODO: Enable testing
 describe('root microservice', () => {
   it('sample test', () => {
-    expect(2).to.equal(1);
+    expect(1).to.equal(1);
   });
 });
