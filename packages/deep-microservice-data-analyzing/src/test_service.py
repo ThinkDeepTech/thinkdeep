d56b0224e4f9b93c49a4e309e11750f0b5f6caf1
haydenmcp <hayden.mcparlane@gmail.com>
2018-11-13 17:35:30 -0600

    testing pre-push hook using python and npm

--- packages/deep-microservice-data-analyzing/src/test_service.py
+++ packages/deep-microservice-data-analyzing/src/test_service.py
@@ -7,3 +7,19 @@ def test_service_name_should_default_to_localhost():
 def test_service_port_should_default_to_3000():
     service = Service()
     assert service.config['host']['port'] == 3000
+
+def test_service_should_allow_user_to_set_host_name():
+    service = Service({
+        'host': {
+            'name': 'different'
+        }
+    })
+    assert service.config['host']['name'] == 'differents'
+
+def test_service_should_allow_user_to_set_host_port():
+    service = Service({
+        'host': {
+            'port': 1234
+        }
+    })
+    assert service.config['host']['port'] == 1234
