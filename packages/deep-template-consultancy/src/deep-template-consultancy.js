dc9d162f5a31ebd788dbee9a2d0937be7e2fc27d
haydenmcp <hayden.mcparlane@gmail.com>
2020-12-10 11:28:22 -0500

    Added pseudo-code to html in template element for high-level stubbing.

--- packages/deep-template-consultancy/src/deep-template-consultancy.js
+++ packages/deep-template-consultancy/src/deep-template-consultancy.js
@@ -1,15 +1,28 @@
-import { html, LitElement, customElement } from 'lit-element';
+import { html, LitElement, customElement, property } from 'lit-element';
+
+import '@polymer/app-layout/';
 
 /* eslint-disable no-unused-vars */
 @customElement('deep-template-consultancy')
 class DeepTemplateConsultancy extends LitElement {
+  /**
+   * Represents site context (state) at any given point.
+   */
+  @property({ type: Object })
+  _context = {};
+
   render() {
     return html`
-      <!-- TODO: Responsive layout -->
 
-      <!-- TODO: Navigation -->
+      <!-- TODO: Include bubble-button-like behavior for floating menu -->
+      <deep-navbar></deep-navbar>
+      <deep-banner></deep-banner>
 
-      <!-- TODO: Background image -->
+      <deep-section>
+
+      </deep-section>
+
+      <deep-footer></deep-footer>
     `;
   }
 }
