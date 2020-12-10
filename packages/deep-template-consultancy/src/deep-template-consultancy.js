40c82beef701526b7e49f15d5337598f1ea735f5
haydenmcp <hayden.mcparlane@gmail.com>
2020-12-10 08:32:24 -0500

    Changed name of site template to consultancy rather than resume.

--- packages/deep-template-consultancy/src/deep-template-consultancy.js
+++ packages/deep-template-consultancy/src/deep-template-consultancy.js
@@ -1,8 +1,8 @@
 import { html, LitElement, customElement } from 'lit-element';
 
 /* eslint-disable no-unused-vars */
-@customElement('deep-template-resume')
-class DeepTemplateResume extends LitElement {
+@customElement('deep-template-consultancy')
+class DeepTemplateConsultancy extends LitElement {
   render() {
     return html`
       <!-- TODO: Responsive layout -->
--- packages/deep-template-resume/src/index.js
+++
@@ -1,1 +0,0 @@
-import './deep-template-resume.js';
