a725c4d3f4ccedba875407059187f3a7cc22757b
haydenmcp <hayden.mcparlane@gmail.com>
2020-12-11 08:29:03 -0500

    Updated template elements to reflect actual desired site structure.

--- packages/deep-template-consultancy/src/deep-template-consultancy.js
+++ packages/deep-template-consultancy/src/deep-template-consultancy.js
@@ -1,38 +1,23 @@
-import { html, LitElement, customElement, property } from 'lit-element';
+import { css, html, LitElement, customElement } from 'lit-element';
 
 /* eslint-disable no-unused-vars */
 @customElement('deep-template-consultancy')
 class DeepTemplateConsultancy extends LitElement {
-  /**
-   * Represents site context (state) at any given point.
-   */
-  @property({ type: Object })
-  _context = {};
+  static get styles() {
+    return css`
+      deep-grid {
+      }
+    `;
+  }
 
   render() {
     return html`
 
-      <deep-grid>
-        <deep-column>
-
-        </deep-column>
-        <deep-column>
-
-        </deep-column>
-        <deep-column>
-
-        </deep-column>
-      </deep-grid>
-
-      <!-- TODO: Include bubble-button-like behavior for floating menu -->
       <deep-navbar></deep-navbar>
       <deep-banner></deep-banner>
-
-      <deep-section>
-
-      </deep-section>
-
+      <deep-section></deep-section>
       <deep-footer></deep-footer>
+
     `;
   }
 }
