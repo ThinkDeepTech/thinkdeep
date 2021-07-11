3a6908250642cea7c37902aa3108746ba7c2b4e5
haydenmcp <hayden.mcparlane@gmail.com>
2021-07-11 11:09:39 -0400

    Working on a fix for karma timing out

--- packages/deep-template-consultancy/karma.conf.js
+++ packages/deep-template-consultancy/karma.conf.js
@@ -8,12 +8,13 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['requirejs', 'mocha', 'chai'],
 
     // plugins to use
     plugins: [
       'karma-mocha',
       'karma-chai',
+      'karma-requirejs',
       'karma-babel-preprocessor',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
@@ -22,7 +23,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       { pattern: '**/*.js ', included: false },
-      { pattern: 'test/**/*.js', included: false },
+      { pattern: 'test/*.js', included: false },
     ],
 
     // list of files / patterns to exclude
@@ -31,8 +32,9 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
     preprocessors: {
+      'test-main.js': ['babel'],
       '**/*.js': ['babel'],
-      'test/**/*.js': ['babel'],
+      'test/*.js': ['babel'],
     },
 
     babelPreprocessor: {
@@ -51,7 +53,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots'],
 
     // web server port
     port: 9876,
@@ -61,14 +63,14 @@ module.exports = function (config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'ChromeHeadless', 'Firefox', 'FirefoxHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -77,5 +79,8 @@ module.exports = function (config) {
     // Concurrency level
     // how many browser instances should be started simultaneously
     concurrency: Infinity,
+
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 60000,
   });
 };
--- packages/deep-template-consultancy/test/deep-template-consultancy.unit.test.js
+++ packages/deep-template-consultancy/test/deep-template-consultancy.unit.test.js
@@ -1,5 +1,13 @@
-// describe('in test', () => {
-//     it('should execute successfully', () => {
-//         assert.equal(true, true);
-//     });
-// });
+const chai = require('chai'),
+  expect = chai.expect;
+
+describe('deep-template-consultancy', () => {
+  // TODO: Pull old attach lib for testing into this project.
+  // beforeEach(() => {
+  //     attach(element);
+  // });
+
+  it('should correctly find this test', () => {
+    expect(true).to.equal(true);
+  });
+});
