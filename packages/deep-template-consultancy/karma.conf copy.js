94b114d9b11d7035ea9f4f1fe602c4d68ee3b065
haydenmcp <hayden.mcparlane@gmail.com>
2021-07-13 08:27:39 -0400

    Modified karma conf and tests are being picked up correctly.

--- packages/deep-template-consultancy/karma.conf copy.js
+++ packages/deep-template-consultancy/karma.conf copy.js
@@ -1,17 +1,18 @@
 // Karma configuration
-// Generated on Tue Jul 13 2021 08:03:13 GMT-0400 (Eastern Daylight Time)
+// Generated on Sat Jul 10 2021 17:35:16 GMT-0400 (Eastern Daylight Time)
 
 module.exports = function (config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: './',
 
     // frameworks to use
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['browserify', 'mocha', 'chai'],
+    frameworks: ['requirejs', 'browserify', 'mocha', 'chai'],
 
     // plugins to use
     plugins: [
+      'karma-requirejs',
       'karma-mocha',
       'karma-chai',
       'karma-babel-preprocessor',
@@ -21,15 +22,34 @@ module.exports = function (config) {
     ],
 
     // list of files / patterns to load in the browser
-    files: ['test/**/*.test.js'],
+    files: [
+      'test-main.js',
+      { pattern: '**/*.js', included: false },
+      { pattern: 'test/**/*.test.js', included: false },
+    ],
 
     // list of files / patterns to exclude
-    exclude: ['node_modules'],
+    exclude: ['node_modules', '**/node_modules/**/*.js', '**/*.conf.js'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
     preprocessors: {
+      'test-main.js': ['browserify', 'babel'],
+      '**/*.js': ['browserify', 'babel'],
       'test/**/*.test.js': ['browserify', 'babel'],
+    },
+
+    babelPreprocessor: {
+      options: {
+        presets: ['@babel/preset-env'],
+        sourceMap: 'inline',
+      },
+      filename: function (file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function (file) {
+        return file.originalPath;
+      },
     },
 
     // test results reporter to use
@@ -52,22 +72,17 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['firefox_private', 'Chrome'],
-
-    customLaunchers: {
-      firefox_private: {
-        base: 'Firefox',
-        flags: ['-private'],
-        displayName: 'Firefox Private Mode',
-      },
-    },
+    browsers: ['Chrome', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser instances should be started simultaneously
     concurrency: Infinity,
+
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 60000,
   });
 };
