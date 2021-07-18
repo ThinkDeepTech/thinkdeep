1a59cbd94e49916855a24b368a01daf2cf8f35ed
haydenmcp <hayden.mcparlane@gmail.com>
2021-07-18 11:20:35 -0400

    Pulling on master branch

--- packages/deep-template-consultancy/test/deep-template-consultancy.unit.test.js
+++ packages/deep-template-consultancy/test/deep-template-consultancy.unit.test.js
@@ -1,12 +1,30 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import '@thinkdeep/deep-template-consultancy/deep-template-consultancy.js';
 
+/**
+ * Find the matching routing component.
+ */
+function findRoute(routes, pageName) {
+  var target = null;
+  for (const route of routes) {
+    if (route.name.includes(pageName)) target = route;
+  }
+  return target;
+}
+
 describe('deep-template-consultancy', () => {
-  it('should include a navigation bar', async () => {
-    const element = await fixture(html`<deep-template-consultancy></deep-template-consultancy>`);
-    // let element = stamp(html`<deep-template-consultancy></deep-template-consultancy>`);
-    // let navbar = element.querySelectorAll('deep-navbar');
-    // expect(navbar).not.to.equal(undefined);
-    expect(element.routes.length).not.to.equal(0);
+  let element;
+  beforeEach(async () => {
+    element = await fixture(html`<deep-template-consultancy></deep-template-consultancy>`);
+  });
+
+  it('should include the home route as /', () => {
+    var target = findRoute(element.routes, 'home');
+    expect(target.path).to.equal('/');
+  });
+
+  it('should include a navbar at the top of the page', () => {
+    const target = element.shadowRoot.querySelectorAll('deep-navbar');
+    expect(target.length).to.equal(1);
   });
 });
