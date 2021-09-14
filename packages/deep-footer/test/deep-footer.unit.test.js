2cbf03eb1275affa79e479c19033686634319ea7
haydenmcp <hayden.mcparlane@gmail.com>
2021-09-14 14:09:06 -0400

    Implemented tests associated with footer work.

--- packages/deep-footer/test/deep-footer.unit.test.js
+++ packages/deep-footer/test/deep-footer.unit.test.js
@@ -1,27 +1,3 @@
-import { html, litFixtureSync, expect } from '@open-wc/testing';
 import '@thinkdeep/deep-footer/deep-footer.js';
 
-describe('deep-footer', () => {
-  const filledCompanyName = 'Sample Company Name';
-  it('should include copywrite date', async () => {
-    const element = await litFixtureSync(
-      html`<deep-footer company="${filledCompanyName}"></deep-footer>`
-    );
-    const copyright = element.shadowRoot.querySelector('.copyright');
-    expect(copyright.textContent).to.contain('\u00A9');
-  });
-
-  it('should display copyright information if the user sets company name', async () => {
-    const element = await litFixtureSync(
-      html`<deep-footer company="${filledCompanyName}"></deep-footer>`
-    );
-    const copyright = element.shadowRoot.querySelector('.copyright');
-    expect(copyright.textContent).to.contain(filledCompanyName);
-  });
-
-  it("should hide the copyright if a company name isn't provided", async () => {
-    const element = await litFixtureSync(html`<deep-footer company=""></deep-footer>`);
-    const copyright = element.shadowRoot.querySelector('.copyright');
-    expect(copyright.textContent).not.to.contain('\u00A9');
-  });
-});
+describe('deep-footer', () => {});
