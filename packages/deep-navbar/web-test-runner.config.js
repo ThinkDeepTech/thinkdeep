591890ea96e073793ae7086825ecc9d36970780e
haydenmcp <hayden.mcparlane@gmail.com>
2021-09-14 17:52:01 -0400

    Testing shared config

--- packages/deep-navbar/web-test-runner.config.js
+++ packages/deep-navbar/web-test-runner.config.js
@@ -1,3 +1,3 @@
-import sharedConfigs from '@thinkdeep/tools/web-test-runner.config';
+const sharedConfigs = require('@thinkdeep/tools/web-test-runner.config');
 
-export default sharedConfigs(10002);
+module.exports = sharedConfigs(10002);
