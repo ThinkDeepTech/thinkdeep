a9619e0dec7992c39e02f769b34770474f143e5a
haydenmcp <hayden.mcparlane@gmail.com>
2021-11-22 15:03:16 -0500

    Began creating the graphql schema for search operation.

--- packages/deep-microservice-data/src/index.js
+++ packages/deep-microservice-data/src/index.js
@@ -1,11 +1,21 @@
-// import { ApolloServer } from 'apollo-server';
-// import typeDefs from './schema.mjs';
+import {ApolloServer} from 'apollo-server';
+import typeDefs from './schema.mjs';
 
+// TODO: Actually pull and populate data from DB. Remove stubbing.
 // import PostgresDataSource from './datasources/PostgresDataSource.mjs'
 
-// const server = new ApolloServer({
-//     typeDefs,
-//     dataSources {
-//         sqlAPI: new PostgresDataSource()
-//     }
-// });
+const server = new ApolloServer({
+  typeDefs,
+  // dataSources {
+  //     sqlAPI: new PostgresDataSource()
+  // }
+});
+
+server.listen().then(() => {
+  // eslint-disable-next-line
+  console.log(`
+        Server is running!
+        Listening on port 4000
+        Explore at https://studio.apollographql.com/sandbox
+    `);
+});
--- packages/deep-microservice-data/src/schema.mjs
+++ packages/deep-microservice-data/src/schema.mjs
@@ -1,10 +1,31 @@
 import { gql } from 'apollo-server';
 
 const typeDefs = gql`
-    type Data {
+
+    # This union will hold all economic node types. I.e, a business, organization
+    union Node = Business | Person
+
+    type Person {
         id: ID!
-        name: String
+        name: String!
+        relationships: [Node]
     }
+
+    type Business {
+        id: ID!
+        name: String!
+        relationships: [Node]
+    }
+
+    type Query {
+        search(businessName: String!): [Node]!
+    }
+
+    # This comment is to track important practices when defining mutations.
+    # - It's recommended to return a type designed specifically for mutations (i.e, a theoretical AddBusiness(...): UpdateBusinessResponse).
+    # - Return modified objects in response so that UI can apply updates to cache, etc without additional query.
+    # type Mutation {
+    # }
 `;
 
 export default typeDefs;
