51a012f40b97ea9b6ece61c1588b8cfa9a8b3583
haydenmcp <hayden.mcparlane@gmail.com>
2021-11-22 19:31:22 -0500

    Refined the knex query and added perms to user to test poc. Graphql sandbox successfully accessed postgres, as well as the necessary tables after access was granted in db.

--- packages/deep-microservice-data/src/datasources/postgres-datasource.mjs
+++ packages/deep-microservice-data/src/datasources/postgres-datasource.mjs
@@ -1,10 +1,15 @@
 import { SQLDataSource } from 'datasource-sql';
 
-const MINUTES = 60;
-
 class PostgresDataSource extends SQLDataSource {
     search(businessName) {
-        return this.knex.select('*').from('edge').where({ name: businessName }).cache();
+        return this.knex.select("*").
+            from('edge').
+            whereIn('first', function() {
+                this.select('id').from('business').where({ name: businessName })
+            }).
+            orWhereIn('second', function() {
+                this.select('id').from('business').where({ name: businessName })
+            }).cache();
     }
 };
 
