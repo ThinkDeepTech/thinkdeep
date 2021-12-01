c9c0b9a467c1a00faba1be4c8486367e3d0d49d8
haydenmcp <hayden.mcparlane@gmail.com>
2021-12-01 15:48:39 -0500

    Began implementation of twitter API fetch, began implementation of sentiment analysis

---
+++ packages/deep-microservice-collection/test/analysis-service.test.mjs
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import mockDb from 'mock-knex';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+const expect = chai.expect;
+chai.use(sinonChai);
+
+import { AnalysisService } from '../src/analysis-service.mjs';
+import { TwitterDataSource } from '../src/datasource/twitter-datasource.mjs';
+
+describe('analysis-service', () => {
+
+    let dataSource;
+    let subject;
+    before((done) => {
+        dataSource = new TwitterDataSource({ client: 'pg' });
+        mockDb.mock(dataSource.knex);
+        TwitterDataSource.prototype.getTweets = sinon.spy();
+        subject = new AnalysisService(dataSource);
+        done();
+    });
+
+    after((done) => {
+        mockDb.unmock(dataSource.knex);
+        done();
+    });
+
+    describe('getSentiment', () => {
+
+    });
+});
