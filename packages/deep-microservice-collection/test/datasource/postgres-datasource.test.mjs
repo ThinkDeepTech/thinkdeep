5a67302b59e08c85644b1a7140f67718bf1be10f
haydenmcp <hayden.mcparlane@gmail.com>
2021-12-01 15:50:37 -0500

    Began implementation of twitter API fetch, began implementation of sentiment analysis

--- packages/deep-microservice-collection/test/datasource/postgres-datasource.test.mjs
+++ packages/deep-microservice-collection/test/datasource/postgres-datasource.test.mjs
@@ -1,75 +1,75 @@
-import chai from 'chai';
-import mockDb from 'mock-knex';
-const expect = chai.expect;
+// import chai from 'chai';
+// import mockDb from 'mock-knex';
+// const expect = chai.expect;
 
-import { PostgresDataSource } from '../../src/datasource/postgres-datasource.mjs';
+// import { TwitterDataSource } from '../../src/datasource/twitter-datasource.mjs';
 
-describe('postgres-datasource', () => {
+// describe('twitter-datasource', () => {
 
-    let subject;
-    before((done) => {
-        subject =  new PostgresDataSource({ client: 'pg' });
-        mockDb.mock(subject.knex);
-        done();
-    });
+//     let subject;
+//     before((done) => {
+//         subject =  new TwitterDataSource({ client: 'pg' });
+//         mockDb.mock(subject.knex);
+//         done();
+//     });
 
-    after((done) => {
-        mockDb.unmock(subject.knex);
-        done();
-    });
+//     after((done) => {
+//         mockDb.unmock(subject.knex);
+//         done();
+//     });
 
-    let tracker;
-    beforeEach((done) => {
-        tracker = mockDb.getTracker();
-        tracker.install();
-        done();
-    });
+//     let tracker;
+//     beforeEach((done) => {
+//         tracker = mockDb.getTracker();
+//         tracker.install();
+//         done();
+//     });
 
-    afterEach((done) => {
-        tracker.uninstall();
-        done();
-    });
+//     afterEach((done) => {
+//         tracker.uninstall();
+//         done();
+//     });
 
-    describe('getBusinessGraph', () => {
+//     describe('getBusinessGraph', () => {
 
-        it('should search the database for all graph edges that relate to business name', (done) => {
-            const expectedBusinessName = 'Some Business';
-            tracker.on('query', (query) => {
-                /**
-                 * NOTE: The try/catch statement below is necessary for the tests to execute properly.
-                 * It's not clear why this is the case. However, more information can be found at the
-                 * following link: https://robporter.ca/tdd-knex-nodejs/
-                 */
-                try {
-                    const actualBusinessName = query.bindings[0];
-                    expect(actualBusinessName).to.equal(expectedBusinessName);
-                    done();
-                } catch (e) {
-                    done(e);
-                }
-                query.response([]);
-            });
+//         it('should search the database for all graph edges that relate to business name', (done) => {
+//             const expectedBusinessName = 'Some Business';
+//             tracker.on('query', (query) => {
+//                 /**
+//                  * NOTE: The try/catch statement below is necessary for the tests to execute properly.
+//                  * It's not clear why this is the case. However, more information can be found at the
+//                  * following link: https://robporter.ca/tdd-knex-nodejs/
+//                  */
+//                 try {
+//                     const actualBusinessName = query.bindings[0];
+//                     expect(actualBusinessName).to.equal(expectedBusinessName);
+//                     done();
+//                 } catch (e) {
+//                     done(e);
+//                 }
+//                 query.response([]);
+//             });
 
-            subject.getBusinessGraph(expectedBusinessName);
-        });
+//             subject.getBusinessGraph(expectedBusinessName);
+//         });
 
-        it('should perform a case-insensitive search for the business name', (done) => {
-            const expectedBusinessName = 'Some Business';
-            tracker.on('query', (query) => {
-                /**
-                 * NOTE: The try/catch statement below is necessary for the tests to execute properly.
-                 * It's not clear why this is the case. However, more information can be found at the
-                 * following link: https://robporter.ca/tdd-knex-nodejs/
-                 */
-                try {
-                    expect(query.sql).to.include(' like ');
-                    done();
-                } catch (e) {
-                    done(e);
-                }
-                query.response([]);
-            });
-            subject.getBusinessGraph(expectedBusinessName);
-        })
-    });
-});
+//         it('should perform a case-insensitive search for the business name', (done) => {
+//             const expectedBusinessName = 'Some Business';
+//             tracker.on('query', (query) => {
+//                 /**
+//                  * NOTE: The try/catch statement below is necessary for the tests to execute properly.
+//                  * It's not clear why this is the case. However, more information can be found at the
+//                  * following link: https://robporter.ca/tdd-knex-nodejs/
+//                  */
+//                 try {
+//                     expect(query.sql).to.include(' like ');
+//                     done();
+//                 } catch (e) {
+//                     done(e);
+//                 }
+//                 query.response([]);
+//             });
+//             subject.getBusinessGraph(expectedBusinessName);
+//         })
+//     });
+// });
