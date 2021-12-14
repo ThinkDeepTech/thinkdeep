5944b08ad1e6e4791b6f1e3f8474ba208954cfa8
haydenmcp <hayden.mcparlane@gmail.com>
2021-12-14 10:07:55 -0500

    Updating tests to use random port.

--- packages/tools/web-test-runner.config.mjs
+++ packages/tools/web-test-runner.config.mjs
@@ -10,7 +10,7 @@ const browsers = {
   firefox: playwrightLauncher({product: 'firefox'}),
 };
 
-const port = await getPort({port: portNumbers(49152, 49252)});
+const port = await getPort();
 
 console.log(`
 
