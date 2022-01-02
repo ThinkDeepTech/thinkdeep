162a2c67cd4a4debd77b0c640c574e003c8b12bb
haydenmcp <hayden.mcparlane@gmail.com>
2022-01-02 15:50:38 -0500

    Added tests around configuration store. Added exceptional cases.

--- packages/deep-microservice-configuration/src/datasource/configuration-store.mjs
+++ packages/deep-microservice-configuration/src/datasource/configuration-store.mjs
@@ -1,23 +1,59 @@
 import {MongoDataSource} from 'apollo-datasource-mongodb';
 
+const validEmail = (userEmail) => {
+    return !!userEmail && (typeof userEmail === 'string')
+}
+
+const validConfiguration = (configuration) => {
+    return !!configuration && !!configuration?.observedEconomicEntities && !!Array.isArray(configuration?.observedEconomicEntities);
+};
+
 class ConfigurationStore extends MongoDataSource {
 
     async configurationExists(userEmail) {
+
+        if(!validEmail(userEmail)) throw new Error(`Determination of an existant configuration requires a valid email. Received: ${userEmail}`);
+
         const configuration = await this.readConfigurationForUser(userEmail);
-        return configuration && configuration?.observedEconomicEntities;
+        return validConfiguration(configuration);
     }
 
     async createConfigurationForUser(userEmail, configuration) {
-        await this.collection.insertOne({ userEmail: userEmail, ...configuration });
+
+        if (!validEmail(userEmail)) throw new Error(`Creating a configuration requires a valid email. Received: ${userEmail}`);
+
+        if (!validConfiguration(configuration)) throw new Error(`Creating a configuration requires a valid configuration. Received: ${JSON.stringify(configuration)}`);
+
+        try {
+            await this.collection.insertOne({ userEmail: userEmail, ...configuration });
+        } catch (e) {
+            throw new Error(`Configuration insertion failed: ${e.message}`);
+        }
     }
 
     async readConfigurationForUser(userEmail) {
-        const configurations = await this.collection.find({userEmail: userEmail}).limit(1).toArray();
-        return configurations[0];
+
+        if (!validEmail(userEmail)) throw new Error(`Reading a configuration requires a valid email. Received: ${userEmail}`);
+
+        try {
+            const configurations = await this.collection.find({userEmail: userEmail}).limit(1).toArray();
+            return configurations[0];
+        } catch (e) {
+            throw new Error(`Configuration read failed: ${e.message}`);
+        }
     }
 
     async updateConfigurationForUser(userEmail, configuration) {
-        await this.collection.updateMany({userEmail: userEmail}, { $set: { observedEconomicEntities: configuration.observedEconomicEntities }});
+
+        if (!validEmail(userEmail)) throw new Error(`Updating a configuration requires a valid email. Received: ${userEmail}`);
+
+        if (!validConfiguration(configuration)) throw new Error(`Updating a configuration requires a valid configuration. Received: ${JSON.stringify(configuration)}`);
+
+        try {
+            await this.collection.updateOne({userEmail: userEmail}, { $set: { observedEconomicEntities: configuration.observedEconomicEntities }});
+        } catch (e) {
+            throw new Error(`Configuration update failed: ${e.message}`);
+        }
     }
 };
 
