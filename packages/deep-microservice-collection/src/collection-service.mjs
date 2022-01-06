50bf63fbc9e9b31c13de2faa1f4488bf534041bb
haydenmcp <hayden.mcparlane@gmail.com>
2022-01-06 11:58:30 -0500

    Updated fetch to occur every 6 hours

--- packages/deep-microservice-collection/src/collection-service.mjs
+++ packages/deep-microservice-collection/src/collection-service.mjs
@@ -9,7 +9,7 @@ import { hasReadAllAccess } from './permissions.mjs';
  * NOTE: Due to twitter developer account limitations only 500,000 tweets can be consumed per month.
  * As a result, ~16000 businesses can be watched.
  */
-const TWITTER_FETCH_INTERVAL = 24 * 60 * 60 * 1000; /** hrs * min * seconds * ms */
+const TWITTER_FETCH_INTERVAL = 6 * 60 * 60 * 1000; /** hrs * min * seconds * ms */
 
 class CollectionService {
 
