e710753cbb26fa99d5915f4da140633e37376b51
haydenmcp <hayden.mcparlane@gmail.com>
2022-02-17 14:47:20 -0500

    Implemented tweet fetch and kafka storage in collect-data cli

--- packages/deep-microservice-collection/cli/collect-data.mjs
+++ packages/deep-microservice-collection/cli/collect-data.mjs
@@ -18,11 +18,20 @@ try {
 
     program.requiredOption('-n, --entity-name <entity name>', 'Specify the name of the economic entity for which the operation will be performed.');
 
-    program.requiredOption('-t, --entity-type <entity type>', 'Specify the type of the economic entity for which the operation will be performed.');
+    program.addOption(
+        new Option('-t, --entity-type <entity type>', 'Specify the type of the economic entity for which the operation will be performed.')
+            .choices(['business'])
+    );
 
     program.parse(process.argv);
 
     const options = program.opts();
+
+    if (!options.operationType)
+        throw new Error('An operation type is required.');
+
+    if (!options.entityType)
+        throw new Error('An entity type is required');
 
     if (options.operationType) {
 
@@ -36,13 +45,21 @@ try {
                     const twitterClient = new TwitterApi(process.env.PREDECOS_TWITTER_BEARER);
                     const readOnlyClient = twitterClient.readOnly;
 
-                    const twitterUrl = `tweets/search/recent?query=${encodeURIComponent(`${options.entityName} lang:en`)}`;
-                    const tweets = readOnlyClient.get(twitterUrl);
+                    debugger;
+
+                    const tweets = await readOnlyClient.v2.get('tweets/search/recent', {
+                        query: `${options.entityName} lang:en`,
+                        max_results: 10
+                    });
+
+                    console.log(`Collected tweets: ${JSON.stringify(tweets)}`);
 
                     const kafka = new Kafka({
                         clientId: 'collect-data',
                         brokers: [`${process.env.PREDECOS_KAFKA_HOST}:${process.env.PREDECOS_KAFKA_PORT}`]
                     });
+
+                    debugger;
 
                     const admin = kafka.admin();
                     const producer = kafka.producer();
@@ -67,9 +84,13 @@ try {
 
                     attachExitHandler(performCleanup);
 
+                    console.log('Connecting to kafka.');
+
                     await admin.connect();
                     await producer.connect();
 
+
+                    console.log('Creating TWEET_FETCHED kafka topic.');
                     // Add data to kafka topic
                     await admin.createTopics({
                         waitForLeaders: true,
@@ -85,6 +106,7 @@ try {
                         tweets
                     };
 
+                    console.log('Sending TWEET_FETCHED event into stream.');
                     producer.send({
                         topic: 'TWEET_FETCHED',
                         messages: [
