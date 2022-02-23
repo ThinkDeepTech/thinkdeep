f3ddcf851e69242d17c7c033b060c85cd1ab7017
haydenmcp <hayden.mcparlane@gmail.com>
2022-02-23 17:38:21 -0500

    Added response to SIGTERM

--- packages/deep-microservice-collection/src/index.mjs
+++ packages/deep-microservice-collection/src/index.mjs
@@ -46,6 +46,9 @@ const attachExitHandler = async (callback) => {
   process.on('SIGINT', () => {
     process.exit(2);
   });
+  process.on('SIGTERM', () => {
+    process.exit(3);
+  });
   process.on('uncaughtException', () => {
     process.exit(99);
   });
