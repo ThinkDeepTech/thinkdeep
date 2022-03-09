6fb72b488bcf29482bae0c553ded0aa8a7622f31
haydenmcp <hayden.mcparlane@gmail.com>
2022-03-09 14:00:50 -0500

    Reverted schedule to every 6 hours for tweet fetch

--- packages/deep-microservice-collection/src/collection-service.mjs
+++ packages/deep-microservice-collection/src/collection-service.mjs
@@ -151,7 +151,7 @@ class CollectionService {
                  * As a result, ~400 businesses can be watched when fetched every 6 hours.
                  */
                  /** min | hour | day | month | weekday */
-                schedule: `* * * * *`,
+                schedule: `0 */6 * * *`,
                 image: 'thinkdeeptech/collect-data:latest',
                 command: 'node',
                 args: ['src/collect-data.mjs', `--entity-name=${entityName}`, `--entity-type=${entityType}`, '--operation-type=fetch-tweets']
