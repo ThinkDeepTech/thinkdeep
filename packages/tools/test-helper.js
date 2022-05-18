b958f090f13f1ce027cbf158f99c213a4ca09691
Hayden McParlane <hayden.mcparlane@gmail.com>
2022-05-18 13:42:21 -0400

    Added tests around summary page

--- packages/tools/test-helper.js
+++ packages/tools/test-helper.js
@@ -1,3 +1,5 @@
+import {litFixtureSync} from '@open-wc/testing';
+
 /**
  * Sleep for the specified number of milliseconds.
  * @param {Number} milliseconds
@@ -24,7 +26,54 @@ const wait = async (milliseconds) => {
  * Therefore, a value << 2000 ms is ideal.
  */
 const delayForPageRender = async () => {
-  await wait(2000);
+  await wait(3000);
 };
 
-export {delayForPageRender, wait, sleep};
+/**
+ * Input a value into an element.
+ * @param {HTMLElement} target Target html element.
+ * @param {any} value Value to assign to target.
+ */
+const input = async (target, value) => {
+  target.value = value;
+  target.dispatchEvent(new Event('input'));
+  await delayForPageRender();
+};
+
+/**
+ * Click the element.
+ * @param {Element} target Element to click.
+ */
+const click = async (target) => {
+  target.click();
+
+  await delayForPageRender();
+};
+
+/**
+ * Select the specified element.
+ *
+ * NOTE: This adds the selected attribute to the element simulating selection.
+ *
+ * @param {Element} target Target element to select.
+ */
+const select = async (target) => {
+  target.setAttribute('selected', '');
+
+  await delayForPageRender();
+};
+
+/**
+ * Create an element fixture for testing.
+ * @param {TemplateResult} template Lit template result from markup.
+ * @return {Element} Element stamped into the dom.
+ */
+const fixtureSync = async (template) => {
+  const element = await litFixtureSync(template);
+
+  await delayForPageRender();
+
+  return element;
+};
+
+export {delayForPageRender, wait, sleep, input, click, select, fixtureSync};
