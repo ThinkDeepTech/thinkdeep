f66fec65449f1342cc7a87b4c7b5caaea0c6e215
Hayden McParlane <hayden.mcparlane@gmail.com>
2022-07-18 10:20:17 -0400

    Modified collection cron to occur every minute.

--- packages/deep-microservice-collection/src/collection-service.js
+++ packages/deep-microservice-collection/src/collection-service.js
@@ -242,7 +242,9 @@ class CollectionService {
            * As a result, ~400 businesses can be watched when fetched every 6 hours.
            */
           /** min | hour | day | month | weekday */
-          schedule: `0 */6 * * *`,
+          schedule: `* * * * *`,
+          // TODO
+          // schedule: `0 */6 * * *`,
           operation: fetchTweets,
         },
         this._k8sClient,
