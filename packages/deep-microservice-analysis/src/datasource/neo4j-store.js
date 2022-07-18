56cc7dde0d4f61b9dae02f156fbb91ca4af44791
Hayden McParlane <hayden.mcparlane@gmail.com>
2022-07-18 17:20:23 -0400

    Testing neo4j aggregate function collect(...)

--- packages/deep-microservice-analysis/src/datasource/neo4j-store.js
+++ packages/deep-microservice-analysis/src/datasource/neo4j-store.js
@@ -81,8 +81,8 @@ class Neo4jStore extends Neo4jDataSource {
     const databaseData = await this.run(
       `
         MATCH (:EconomicEntity { name: $entityName, type: $entityType}) -[:OPERATED_ON]-> (dateTime:DateTime) -[:RECEIVED_DATA]-> (tweet:Data { type: "tweet" }) -[:RECEIVED_MEASUREMENT]-> (sentiment:Sentiment)
-        WITH collect(dateTime), tweet, sentiment
-        RETURN apoc.date.format(dateTime.value.epochMillis, 'ms', "yyyy-MM-dd'T'HH:mm:ss'Z'") as utcDateTime, tweet, sentiment
+        WITH collect(dateTime.value.epochMillis) as dateMilliseconds, tweet, sentiment
+        RETURN apoc.date.format(dateMilliseconds, 'ms', "yyyy-MM-dd'T'HH:mm:ss'Z'") as utcDateTime, tweet, sentiment
         ORDER BY dateTime.value DESC
         LIMIT 1
       `,
